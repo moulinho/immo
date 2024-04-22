09fcdb91ead1c8f572cc465d6477694f318fe225
moulinho <moulinho132@gmail.com>
2024-04-22 23:36:52 +0000

    fermerture du site

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div
+    class="fullscreen bg-amber text-white text-center q-pa-md flex flex-center"
+  >
     <div>
-      <div style="font-size: 30vh">
-        404
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.7">
+        site web désactivé web pour le non-renouvellement <br />
+        de votre abonnement
       </div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
+      <!-- <q-btn
         class="q-mt-xl"
         color="white"
         text-color="blue"
@@ -17,13 +18,13 @@
         to="/"
         label="Go Home"
         no-caps
-      />
+      /> -->
     </div>
   </div>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'ErrorNotFound'
+  name: "ErrorNotFound",
 });
 </script>
